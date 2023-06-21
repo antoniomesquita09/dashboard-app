c13009abd2fa376e0ae2b9e29925b99ab8637e08
Antonio Mesquita Junior <antonio.mesquita@ifood.com.br>
2023-06-21 11:32:00 -0300

    feat: fetch cpu and memory data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+
 import SimpleLineChart from '@/components/chart'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+const queryClient = new QueryClient()
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SimpleLineChart />
+      <QueryClientProvider client={queryClient}>
+        <SimpleLineChart />
+      </QueryClientProvider>
     </main>
   )
 }
--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import axios from "axios";
 import React from "react";
+import { useQuery, useQueryClient } from "react-query";
 import {
   LineChart,
   Line,
@@ -56,33 +58,119 @@ const data = [
     },
   ];
 
+const fetchCPU = async () => {
+  const { data } = await axios.get("http://localhost:8081/cpu");
+  return data;
+};
+const fetchMemory = async () => {
+  const { data } = await axios.get("http://localhost:8081/memory");
+  return data;
+};
+// const fetchKubernetes = async () => {
+//   const { data } = await axios.get("http://localhost:8081/kubernetes");
+//   return data;
+// };
+
 const SimpleLineChart = () => {
+
+  const queryClient = useQueryClient()
+  
+  const { data: cpus } = useQuery('cpu', fetchCPU)
+  const { data: memories } = useQuery('memory', fetchMemory)
+  // const { data: kubernetes, isLoading } = useQuery('kubernetes', fetchKubernetes)
+  
   return (
-    <LineChart
-      width={600}
-      height={400}
-      data={data}
-      margin={{
-        top: 15,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="pv"
-        stroke="#8884d8"
-        // activeDot={{ r: 8 }}
-      />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      <Line type="monotone" dataKey="amt" stroke="#ff0000" />
-    </LineChart>
+    <>
+      <h2>CPU Chart</h2>
+
+      <LineChart
+        width={600}
+        height={400}
+        data={cpus}
+        margin={{
+          top: 15,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date_time" />
+        <YAxis
+          dataKey="percentage"
+          label={{
+            value: "%",
+            style: { textAnchor: 'middle' },
+            // angle: -90,
+            position: 'left',
+            offset: 0,
+          }}
+        />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="percentage" stroke="#82ca9d" />
+      </LineChart>
+
+      <hr style={{ borderTop: "3px solid #bbb", margin: "20px 0", width: "100%" }} />
+      <h2>Memory Chart</h2>
+      
+      <LineChart
+        width={600}
+        height={400}
+        data={memories}
+        margin={{
+          top: 15,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date_time" />
+        <YAxis
+          label={{
+            value: "MB",
+            style: { textAnchor: 'middle' },
+            position: 'left',
+            offset: 0,
+          }}
+        />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="used" stroke="#ff0000" />
+        {/* <Line type="monotone" dataKey="total" stroke="#82ca9d" /> */}
+        <Line type="monotone" dataKey="committed" stroke="#8884d8" />
+      </LineChart>
+
+      <hr style={{ borderTop: "3px solid #bbb", margin: "20px 0", width: "100%" }} />
+      <h2>Kubernetes Chart</h2>
+
+      <LineChart
+        width={600}
+        height={400}
+        data={cpus}
+        margin={{
+          top: 15,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="percentage" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {/* <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          // activeDot={{ r: 8 }}
+        /> */}
+        <Line type="monotone" dataKey="percentage" stroke="#82ca9d" />
+        {/* <Line type="monotone" dataKey="amt" stroke="#ff0000" /> */}
+      </LineChart>
+    </>
   );
 };
 
